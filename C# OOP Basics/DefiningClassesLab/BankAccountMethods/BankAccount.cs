8604653381b97bc18028ffe0b49f9223ff3ab00e
blazarow09 <blazarow09@gmail.com>
2018-08-01 13:02:07 +0300

    update dir

---
+++ C# OOP Basics/DefiningClassesLab/BankAccountMethods/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿public class BankAccount
+{
+    private int Id;
+
+    public int ID
+    {
+        get { return Id; }
+        set { Id = value; }
+    }
+
+    private decimal  balance;
+
+    public decimal  Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw (decimal amount)
+    {
+        this.balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account {this.ID}, balance {this.Balance}";
+    }
+}
---
+++ C# OOP Basics/DefiningClassesLab/TestClient/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿class BankAccount
+{
+    private int Id;
+
+    public int ID
+    {
+        get { return Id; }
+        set { Id = value; }
+    }
+
+    private decimal  balance;
+
+    public decimal  Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw (decimal amount)
+    {
+        if (Balance < amount)
+        {
+            System.Console.WriteLine("Insufficient balance");
+        }
+        else
+        {
+        Balance -= amount;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.ID}, balance {this.Balance:f2}";
+    }
+}
+

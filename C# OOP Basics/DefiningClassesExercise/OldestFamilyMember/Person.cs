21a40cdf8fb7a5062ed3f593f89109afc8b47f63
blazarow09 <blazarow09@gmail.com>
2018-08-01 15:16:19 +0300

    upload OldestFamilyMember problem solve

---
+++ C# OOP Basics/DefiningClassesExercise/OldestFamilyMember/Person.cs
@@ -0,0 +1,23 @@
+﻿public class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+}

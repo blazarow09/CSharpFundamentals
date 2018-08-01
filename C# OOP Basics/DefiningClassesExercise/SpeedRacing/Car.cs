35abfc62c028b0e54704648e439bc435fdeb227c
blazarow09 <blazarow09@gmail.com>
2018-08-01 17:26:27 +0300

    fixed StartUp.cs name and upload SpeedRacing problem solve

---
+++ C# OOP Basics/DefiningClassesExercise/SpeedRacing/Car.cs
@@ -0,0 +1,30 @@
+﻿    public class Car
+    {
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double Consumption { get; set; }
+    public double Distance { get; set; }
+
+    public Car(string model, double fuelAmount, double conspumtion)
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAmount;
+        this.Consumption = conspumtion;
+        this.Distance = 0;
+    }
+
+    public bool Drive(double distance)
+    {
+        double fuelNeeded = distance * this.Consumption;
+
+        if (this.FuelAmount < fuelNeeded)
+        {
+            return false;
+        }
+
+        this.FuelAmount -= fuelNeeded;
+        this.Distance += distance;
+        return true;
+    }
+}
+

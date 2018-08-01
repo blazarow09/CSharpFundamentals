64ac16babf2a73c264026975ff5cd40da9829302
blazarow09 <blazarow09@gmail.com>
2018-08-01 11:49:33 +0300

    upload BankAccountMethods problem solve

---
+++ C# OOP Basics/DefiningClassesLab/BankAccountMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankAccountMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new BankAccount();
+
+            acc.ID = 1;
+            acc.Deposit(15);
+            acc.Withdraw(10);
+
+            Console.WriteLine(acc);
+        }
+    }
+}

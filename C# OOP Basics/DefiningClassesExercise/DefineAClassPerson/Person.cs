7ad9f2885c2d727d8ec549e73a1fc8b08924f65b
blazarow09 <blazarow09@gmail.com>
2018-08-01 14:07:56 +0300

    upload DefineAClassPerson problem solve

---
+++ C# OOP Basics/DefiningClassesExercise/DefineAClassPerson/Person.cs
@@ -0,0 +1,20 @@
+﻿public class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    public int MyProperty
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+}
+

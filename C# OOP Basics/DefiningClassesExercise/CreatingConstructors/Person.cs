c501f069d11b612e1eaf47b52b187db9a2dc2b05
blazarow09 <blazarow09@gmail.com>
2018-08-01 14:48:59 +0300

    upload CreateConstructor problem solve

---
+++ C# OOP Basics/DefiningClassesExercise/CreatingConstructors/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    public Person() : this("No name", 1)
+    {
+
+    }
+
+    public Person(int age) : this("No name", age)
+    {
+
+    }
+
+    public Person(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new NullReferenceException("Invalid name");
+        }
+        this.name = name;
+    }
+
+    public Person(string name, int age) : this(name)
+    {
+        this.age = age;
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+    }
+}
---
+++ C# OOP Basics/DefiningClassesExercise/CreatingConstructors/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+        ConstructorInfo emptyCtor = personType.GetConstructor(new Type[] { });
+        ConstructorInfo ageCtor = personType.GetConstructor(new[] { typeof(int) });
+        ConstructorInfo nameAgeCtor = personType.GetConstructor(new[] { typeof(string), typeof(int) });
+
+        bool isSwapped = false;
+
+        if (nameAgeCtor == null)
+        {
+            nameAgeCtor = personType.GetConstructor(new[] { typeof(int), typeof(string) });
+            isSwapped = true;
+        }
+
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        Person basePerson = (Person)emptyCtor.Invoke(new object[] { });
+        Person personWithAge = (Person)ageCtor.Invoke(new object[] { age });
+        Person personWithAgeAndName = isSwapped ? (Person)nameAgeCtor.Invoke(new object[] { age, name }) : (Person)nameAgeCtor.Invoke(new object[] { name, age });
+
+        Console.WriteLine("{0} {1}", basePerson.Name, basePerson.Age);
+        Console.WriteLine("{0} {1}", personWithAge.Name, personWithAge.Age);
+        Console.WriteLine("{0} {1}", personWithAgeAndName.Name, personWithAgeAndName.Age);
+    }
+}
+

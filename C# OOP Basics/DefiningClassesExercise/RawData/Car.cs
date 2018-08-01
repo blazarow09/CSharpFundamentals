85ffc74800cbe915cf7f5db62f7145fc1f40a565
blazarow09 <blazarow09@gmail.com>
2018-08-01 17:45:30 +0300

    upload RawData problem solve

---
+++ C# OOP Basics/DefiningClassesExercise/RawData/Car.cs
@@ -0,0 +1,45 @@
+﻿public class Car
+{
+    private string model;
+    private Engine engine;
+    private Cargo cargo;
+    private Tire[] tires;
+
+    public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+    {
+        this.model = model;
+        this.engine = engine;
+        this.cargo = cargo;
+        this.tires = tires;
+    }
+
+    public Tire[] Tires
+    {
+        get { return tires; }
+        set { tires = value; }
+    }
+
+
+    public Cargo Cargo
+    {
+        get { return cargo; }
+        set { cargo = value; }
+    }
+
+
+    public Engine Engine
+    {
+        get { return engine; }
+        set { engine = value; }
+    }
+
+
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+}
+

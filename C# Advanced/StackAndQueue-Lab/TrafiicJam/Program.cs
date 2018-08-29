e42526a051c24a90f5b8ef28c97c3c83e18eabb9
blazarow09 <blazarow09@gmail.com>
2018-08-29 21:40:40 +0300

    upload TrafiicJam solve

---
+++ C# Advanced/StackAndQueue-Lab/TrafiicJam/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafiicJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carsPerGreen = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine();
+
+            var cars = new Queue<string>();
+
+            var countCarsPassed = 0;
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < carsPerGreen; i++)
+                    {
+                        if (cars.Count < carsPerGreen)
+                        {
+                            for (int j = 0; j < cars.Count; j++)
+                            {
+                                Console.WriteLine($"{cars.Dequeue()} passed!");
+                                countCarsPassed++;
+                            }
+                        }
+                        else
+                        {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        countCarsPassed++;
+                        }
+                    }
+                }
+                else
+                {
+                        cars.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{countCarsPassed} cars passed the crossroads.");
+        }
+    }
+}

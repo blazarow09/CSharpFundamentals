45112961a986100502dd0500b0060948630f3e0d
unknown <blazarow09@gmail.com>
2018-09-17 18:04:24 +0300

    123

---
+++ C# Advanced/StackAndQueueExercise/ReverseNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var stack = new Stack<string>(input.Split());
+
+            foreach (var item in stack)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+            
+
+        }
+    }
+}

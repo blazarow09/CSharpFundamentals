105e5f20b9f91bd3e6d506239bd51bd9ec48a288
blazarow09 <blazarow09@gmail.com>
2018-09-19 02:39:13 +0300

    upload CalculateSequenceWithQueue solve

---
+++ C# Advanced/StackAndQueueExercise/CalculateSequenceWithQueue/CalculateSequenceWithQueue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculateSequenceWithQueue
+{
+    class CalculateSequenceWithQueue
+    {
+        static void Main(string[] args)
+        {
+            var number = long.Parse(Console.ReadLine());
+
+            var queue = new Queue<long>();
+            var counter = 0;
+
+            queue.Enqueue(number);
+
+            while (counter < 50)
+            {
+                var currentNumber = queue.Peek();
+
+                queue.Enqueue(currentNumber + 1);
+                queue.Enqueue(2 * currentNumber + 1);
+                queue.Enqueue(currentNumber + 2);
+
+                Console.Write(queue.Dequeue() + " ");
+
+                counter++;
+            }
+        }
+    }
+}

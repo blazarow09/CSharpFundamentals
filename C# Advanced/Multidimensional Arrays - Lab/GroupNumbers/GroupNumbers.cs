c79beebcd06e03bc56631a32f4ac47445ba8d429
blazarow09 <blazarow09@gmail.com>
2018-09-19 14:23:37 +0300

    upload GroupNumbers solve

---
+++ C# Advanced/Multidimensional Arrays - Lab/GroupNumbers/GroupNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace GroupNumbers
+{
+    class GroupNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[][] jaggedArray = new int[3][];
+            int[] sizes = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sizes[Math.Abs(numbers[i] % 3)]++;
+            }
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                jaggedArray[i] = new int[sizes[i]];
+            }
+
+            int[] indices = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int remainder = Math.Abs(numbers[i] % 3);
+
+                jaggedArray[remainder][indices[remainder]++] = numbers[i];
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", jaggedArray[i]));
+            }
+        }
+    }
+}

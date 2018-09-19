dee0542219878dec809129543c9d2aec2f9ba557
blazarow09 <blazarow09@gmail.com>
2018-09-19 13:41:54 +0300

    upload SumMatrixElements solve

---
+++ C# Advanced/Multidimensional Arrays - Lab/SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,45 @@
+﻿namespace SumMatrixElements
+{
+    using System;
+    using System.Linq;
+
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            var rowsAndCols = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[rowsAndCols[0], rowsAndCols[1]];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                var rowValues = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = rowValues[cols];
+                }
+            }
+
+            int sumOfElements = 0;
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    sumOfElements += matrix[rows, cols];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sumOfElements);
+        }
+    }
+}

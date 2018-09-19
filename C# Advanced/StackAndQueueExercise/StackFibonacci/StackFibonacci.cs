5d5c89a62e14c6a45feeed6b92d1b323401eca17
blazarow09 <blazarow09@gmail.com>
2018-09-19 11:58:24 +0300

    upload StackFibonacci solve

---
+++ C# Advanced/StackAndQueueExercise/StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackFibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            var stack = new Stack<long>();
+            stack.Push(0);
+            stack.Push(1);
+
+            for (int i = 2; i <= N; i++)
+            {
+                long second = stack.Pop();
+                long first = stack.Pop();
+
+                stack.Push(second);
+                stack.Push(first + second);
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}

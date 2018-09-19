83f8b4b39937093dae62b94e80d60d8d4d10f4f4
blazarow09 <blazarow09@gmail.com>
2018-09-19 03:28:29 +0300

    upload TruckToor solve

---
+++ C# Advanced/StackAndQueueExercise/TruckToor/TruckToor.cs
@@ -0,0 +1,36 @@
+﻿namespace TruckToor
+{
+    using System;
+    using System.Linq;
+
+    class TruckToor
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int smallestIndex = 0;
+            int fuel = 0;
+
+            for (int index = 0; index < N; index++)
+            {
+                var input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var amountOfPetrol = input[0];
+                var distance = input[1];
+
+                fuel += amountOfPetrol - distance;
+
+                if (fuel < 0)
+                {
+                    fuel = 0;
+                    smallestIndex = index + 1;
+                }
+            }
+            Console.WriteLine(smallestIndex);
+        }
+    }
+}

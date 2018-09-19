e0a39cbf10884ac61fe9186ae3d0850541a8ed79
blazarow09 <blazarow09@gmail.com>
2018-09-19 13:41:21 +0300

    upload PrintingTwoDimensionMatrix solve

---
+++ C# Advanced/Multidimensional Arrays - Lab/PrintingTwoDimensionMatrix/PrintingTwoDimensionMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrintingTwoDimensionMatrix
+{
+    class PrintingTwoDimensionMatrix
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8}
+            };
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.WriteLine(matrix[rows, cols]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

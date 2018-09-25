b9ac3eccaed5783297ea2b9dc0f109f7032afe5a
blazarow09 <blazarow09@gmail.com>
2018-09-25 15:40:18 +0300

    upload 2x2SquaresInMatrix solve

---
+++ C# Advanced/MultidimensionalArrays - Exercise/2x2SquaresInMatrix/2x2SquaresInMatrix.cs
@@ -0,0 +1,50 @@
+﻿namespace _2x2SquaresInMatrix
+{
+    using System;
+    using System.Linq;
+
+    class _2x2SquaresInMatrix
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = sizes[0];
+            var cols = sizes[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var inputLetters = Console.ReadLine()
+                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                     .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputLetters[col];
+                }
+            }
+
+            var isEqual = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row + 1, col] == matrix[row + 1, col + 1]
+                        && matrix[row + 1, col + 1] == matrix[row, col + 1])
+                    {
+                        isEqual++;
+                    }
+                }
+            }
+            Console.WriteLine(isEqual);
+        }
+    }
+}

a76380066cb8bff765aa7a146de07158ac12cc2c
blazarow09 <blazarow09@gmail.com>
2018-09-28 02:16:58 +0300

    upload ProductShop solve

---
+++ C# Advanced/DictionariesAndSetsAdvanced - Lab/ProductShop/ProductShop.cs
@@ -0,0 +1,48 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ProductShop
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var dictionary = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (tokens[0] != "Revision")
+            {
+                var shop = tokens[0];
+                var product = tokens[1];
+                var price = double.Parse(tokens[2]);
+
+                if (dictionary.ContainsKey(shop) == false)
+                {
+                    dictionary[shop] = new Dictionary<string, double>
+                    {
+                        { product, price }
+                    };
+                }
+                else if (dictionary.ContainsKey(shop))
+                {
+                    dictionary[shop].Add(product, price);
+                }
+
+                tokens = Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key}->");
+
+                foreach (var product in item.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}

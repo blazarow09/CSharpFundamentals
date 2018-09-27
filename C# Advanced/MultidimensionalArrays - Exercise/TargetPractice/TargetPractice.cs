28bbbb8134a2778d1406c8625531f61cef55f9c1
blazarow09 <blazarow09@gmail.com>
2018-09-27 23:06:34 +0300

    upload AverageStudentsGrades solve

---
+++ C# Advanced/MultidimensionalArrays - Exercise/TargetPractice/TargetPractice.cs
@@ -0,0 +1,98 @@
+﻿namespace TargetPractice
+{
+    using System;
+    using System.Linq;
+
+    class TargetPractice
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = sizes[0];
+            var cols = sizes[1];
+
+            char[][] matrix = new char[rows][];
+
+            var snake = Console.ReadLine()
+                .ToCharArray();
+
+            var count = 0;
+
+            for (int row = rows - 1; row >= 0; row--)
+            {
+                matrix[row] = new char[cols];
+                for (int col = cols - 1; col >= 0; col--)
+                {
+                    if (count == snake.Length)
+                    {
+                        count = 0;
+                    }
+
+                    matrix[row][col] = snake[count];
+                    count++;
+                }
+            }
+
+            var cmdArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var impactRow = cmdArgs[0];
+            var impactCol = cmdArgs[1];
+            var radius = cmdArgs[2];
+
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (IsShooted(row, col, impactRow, impactCol, radius))
+                    {
+                        matrix[row][col] = ' ';
+                    }
+                }
+            }
+
+            for (int col = 0; col < matrix[0].Length; col++)
+            {
+                for (int row = matrix.Length - 1; row >= 0; row--)
+                {
+                    if (matrix[row][col] == ' ' 
+                        && matrix[row - 1][col] != ' ')
+                    {
+                        CellFalling(matrix, row, col);
+                    }
+                }
+            }
+        }
+
+        private static bool IsShooted(int row, int col, int impactRow, int impactCol, int radius)
+        {
+            double distance = Math.Sqrt((row - impactRow) * (row - impactRow));
+            return distance <= radius;
+        }
+
+        private static void CellFalling(char[][] matrix, int row, int col)
+        {
+            while (row < matrix.Length)
+            {
+                if (matrix[row][col] == ' ')
+                {
+                    char temporary = matrix[row - 1][col];
+                    matrix[row - 1][col] = matrix[row][col];
+                    matrix[row][col] = temporary;
+                    row++;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/SetsAndDictionariesAdvanced - Lab/AverageStudentsGrades/AverageStudentsGrades.cs
@@ -0,0 +1,45 @@
+﻿namespace RecordUniqueNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class RecordUniqueNames
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            var dictionary = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var name = line[0];
+                var grade = double.Parse(line[1]);
+
+                if (dictionary.ContainsKey(name) == false)
+                {
+                    dictionary[name] = new List<double>() { grade };
+                }
+                else
+                {
+                    dictionary[name].Add(grade);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.Write($"{item.Key} -> ");
+
+                foreach (var grades in item.Value)
+                {
+                    Console.Write($"{ grades:f2} ");
+                }
+                Console.WriteLine($"(avg: {item.Value.Average():F2})");
+            }
+        }
+    }
+}

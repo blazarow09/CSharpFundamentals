f8dcec27a977e28990e6a9a6a46ac6ea7fe3f361
blazarow09 <blazarow09@gmail.com>
2018-09-28 03:37:07 +0300

    upload UniqueElements solve

---
+++ C# Advanced/DictionariesAndSetsAdvanced - Exercise/UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,27 @@
+﻿namespace UniqueUsernames
+{
+    using System;
+    using System.Collections.Generic;
+
+    class UniqueUsernames
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var usernames = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var username = Console.ReadLine();
+
+                usernames.Add(username);
+            }
+
+            foreach (var username in usernames)
+            {
+                Console.WriteLine($"{username}");
+            }
+        }
+    }
+}

2d54fe3430a868cb40dd6f01e50360c5540a8488
blazarow09 <blazarow09@gmail.com>
2018-09-28 03:44:48 +0300

    upload PeriodicTable solve

---
+++ C# Advanced/DictionariesAndSetsAdvanced - Exercise/PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,29 @@
+﻿namespace PeriodicTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class PeriodicTable
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var periodicTable = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var element in line)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", periodicTable.OrderBy(e => e)));
+        }
+    }
+}

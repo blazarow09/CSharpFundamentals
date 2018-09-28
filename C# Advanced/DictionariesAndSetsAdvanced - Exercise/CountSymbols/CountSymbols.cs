19756b5f1f6cd304c68fff580c818a6902784473
blazarow09 <blazarow09@gmail.com>
2018-09-28 04:34:20 +0300

    upload CountSymbols solve

---
+++ C# Advanced/DictionariesAndSetsAdvanced - Exercise/CountSymbols/CountSymbols.cs
@@ -0,0 +1,34 @@
+﻿namespace CountSymbols
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine()
+                .ToCharArray();
+
+            var dictionary = new Dictionary<char, int>();
+
+            foreach (var ch in line)
+            {
+                if (dictionary.ContainsKey(ch) == false)
+                {
+                    dictionary.Add(ch, 1);
+                }
+                else if (dictionary.ContainsKey(ch))
+                {
+                    dictionary[ch]++;
+                }
+            }
+
+            foreach (var ch in dictionary.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}

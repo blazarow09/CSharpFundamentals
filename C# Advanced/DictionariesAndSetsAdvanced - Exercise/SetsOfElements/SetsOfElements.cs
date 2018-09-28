b2b24d42da5fe94719df5f3dab52a7abd0050784
blazarow09 <blazarow09@gmail.com>
2018-09-28 03:36:57 +0300

    upload SetsOfElements solve

---
+++ C# Advanced/DictionariesAndSetsAdvanced - Exercise/SetsOfElements/SetsOfElements.cs
@@ -0,0 +1,47 @@
+﻿namespace SetsOfElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SetsOfElements
+    {
+        static void Main(string[] args)
+        {
+            var sizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstSet = new HashSet<double>();
+
+            var secondSet = new HashSet<double>();
+
+            for (int i = 0; i < sizes[0]; i++)
+            {
+                var line = double.Parse(Console.ReadLine());
+
+                firstSet.Add(line);
+            }
+
+            for (int i = 0; i < sizes[1]; i++)
+            {
+                var line = double.Parse(Console.ReadLine());
+
+                secondSet.Add(line);
+            }
+
+            var print = new List<double>();
+
+            foreach (var num in firstSet)
+            {
+                if (secondSet.Contains(num))
+                {
+                    print.Add(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", print));
+        }
+    }
+}

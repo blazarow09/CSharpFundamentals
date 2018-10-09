aac2d5f5eb936aecd8106157da77bf59fb875868
blazarow09 <blazarow09@gmail.com>
2018-10-10 02:13:04 +0300

    uplaod BankAccount solve

---
+++ C# OOP Basics/DefinningClasses - Lab/BankAccount/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+
+        account.Id = 1;
+        account.Deposit(15);
+        account.Withdraw(10);
+
+        Console.WriteLine(account);
+    }
+}
+

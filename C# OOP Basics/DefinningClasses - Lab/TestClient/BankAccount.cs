aef419eaead797c8be1eebc229b7c187d993b0b2
blazarow09 <blazarow09@gmail.com>
2018-10-10 02:50:20 +0300

    upload TestClient solve

---
+++ C# OOP Basics/DefinningClasses - Lab/TestClient/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class BankAccount
+{
+    private int id;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private decimal balance;
+
+    public decimal Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void WithDraw(decimal amount)
+    {
+        if (Balance < amount)
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+        else
+        {
+            Balance -= amount;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.Id}, balance {this.Balance:f2}";
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Lab/TestClient/TestClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TestClient
+{
+    static void Main(string[] args)
+    {
+        var accounts = new Dictionary<int, BankAccount>();
+
+        var line = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        while (line[0] != "End")
+        {
+            var command = line[0];
+            var accId = int.Parse(line[1]);
+
+            switch (command)
+            {
+                case "Create":
+                    CreateAccount(line, accounts, accId);
+                    break;
+                case "Deposit":
+                    DepositToAccount(line, accounts, accId);
+                    break;
+                case "Withdraw":
+                    WithdrawFromAccount(line, accounts, accId);
+                    break;
+                case "Print":
+                    PrintAccount(line, accounts, accId);
+                    break;
+                default:
+                    break;
+            }
+
+            line = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+
+    private static void PrintAccount(string[] line, Dictionary<int, BankAccount> accounts, int accId)
+    {
+        Console.WriteLine(accounts[accId].ToString());
+    }
+
+    private static void WithdrawFromAccount(string[] line, Dictionary<int, BankAccount> accounts, int accId)
+    {
+        if (IsAccountValid(accId, accounts))
+        {
+            var amount = int.Parse(line[2]);
+            accounts[accId].WithDraw(amount);
+        }
+    }
+
+    private static void DepositToAccount(string[] line, Dictionary<int, BankAccount> accounts, int accId)
+    {
+        if (IsAccountValid(accId, accounts))
+        {
+            var amount = int.Parse(line[2]);
+            accounts[accId].Deposit(amount);
+        }
+    }
+
+    private static void CreateAccount(string[] line, Dictionary<int, BankAccount> accounts, int accId)
+    {
+        if (accounts.ContainsKey(accId))
+        {
+            Console.WriteLine("Account already exists");
+        }
+        else
+        {
+            var acc = new BankAccount();
+            acc.Id = accId;
+            accounts.Add(accId, acc);
+        }
+    }
+
+    private static bool IsAccountValid(int accId, Dictionary<int, BankAccount> accounts)
+    {
+        if (accounts.ContainsKey(accId))
+        {
+            return true;
+        }
+
+        Console.WriteLine("Account does not exist");
+        return false;
+    }
+}

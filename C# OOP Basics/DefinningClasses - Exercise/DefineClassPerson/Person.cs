5b987ede8ecd2636ab3a2b1ad6e99ea6089b4a51
blazarow09 <blazarow09@gmail.com>
2018-10-10 15:08:29 +0300

    upload DefinePersonClass solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/DefineClassPerson/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Person
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name}, Age {this.Age}";
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/DefineClassPerson/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        person.Name = Console.ReadLine();
+        person.Age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(person.ToString());
+    }
+}

b53dfde7152a832bc6b658dc385118fbd5012ceb
blazarow09 <blazarow09@gmail.com>
2018-10-11 22:25:42 +0300

    update DateModifier solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/SpeedRacing/Car.cs
@@ -0,0 +1,36 @@
+﻿public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double Conspumption;
+
+    public string Model { get; set; }
+
+    public double FuelAmount { get; set; }
+
+    public double Consumption { get; set; }
+
+    public double Distance { get; set; }
+
+    public Car(string model, double fuelAmount, double fuelConsumption)
+    {
+        this.Model = model;
+        this.FuelAmount = fuelAmount;
+        this.Consumption = fuelConsumption;
+        this.Distance = 0;
+    }
+
+    public bool Drive(double distance)
+    {
+        var fuelNeeded = distance * this.Consumption;
+
+        if (this.FuelAmount < fuelNeeded)
+        {
+            return false;
+        }
+
+        this.FuelAmount -= fuelNeeded;
+        this.Distance += distance;
+        return true;
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/SpeedRacing/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var cars = new List<Car>();
+
+        var carsCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < carsCount; i++)
+        {
+            var carTokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var carModel = carTokens[0];
+            
+            if (cars.Any(c => c.Model == carModel) == false)
+            {
+                Car car = new Car(carModel,
+                    double.Parse(carTokens[1]),
+                    double.Parse(carTokens[2]));
+                cars.Add(car);
+            }
+        }
+
+        var line = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        while (line[0] != "End")
+        {
+            var carModel = line[1];
+            var km = int.Parse(line[2]);
+
+            var carToDrive = cars.Find(c => c.Model == carModel);
+
+            bool isMoved = carToDrive.Drive(km);
+
+            if (isMoved == false)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+
+            line = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount} {car.Distance}");
+        }
+    }
+}

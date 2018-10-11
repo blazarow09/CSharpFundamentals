94bb279e3e3943d0335e8e75f97aade10e7b95a9
blazarow09 <blazarow09@gmail.com>
2018-10-11 13:54:19 +0300

    update solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Family
+{
+    private string name;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        var familyBook = new Dictionary<string, Family>();
+
+        var count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            AddMember(count, familyBook);
+        }
+
+        GetOldestMember(familyBook);
+    }
+
+    private static void GetOldestMember(Dictionary<string, Family> familyBook)
+    {
+        var oldestMemberAge = 0;
+        var oldestMemberName = "";
+
+        foreach (var member in familyBook)
+        {
+            if (member.Value.Age > oldestMemberAge)
+            {
+                oldestMemberName = member.Value.Name;
+                oldestMemberAge = member.Value.Age;
+            }
+        }
+
+        Console.WriteLine($"{oldestMemberName} {oldestMemberAge}");
+    }
+
+    private static void AddMember(int count, Dictionary<string, Family> familyBook)
+    {
+        var line = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        var name = line[0];
+        var age = int.Parse(line[1]);
+
+        if (familyBook.ContainsKey(name) == false)
+        {
+            var family = new Family();
+            family.Name = name;
+            family.Age = age;
+            familyBook.Add(name, family);
+        }
+    }
+}

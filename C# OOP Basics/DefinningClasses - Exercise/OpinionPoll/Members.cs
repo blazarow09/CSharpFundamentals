53bd75383d5941391d065a464cfab2a0cb7be330
blazarow09 <blazarow09@gmail.com>
2018-10-11 16:13:51 +0300

    uplaod OpinionPoll solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/OpinionPoll/Members.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class Members
+{
+    private List<Person> people;
+
+    public Members()
+    {
+        this.people = new List<Person>();
+    }
+
+    public List<Person> People
+    {
+        get { return this.people; }
+        set { this.people = value; }
+    }
+
+    public void AddMember(Person member)
+    {
+        this.people.Add(member);
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/OpinionPoll/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace OpinionPoll
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var members = new Members();
+
+            var membersCount = int.Parse(Console.ReadLine());
+
+            while (membersCount > 0)
+            {
+                var personData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var member = new Person(personData[0], int.Parse(personData[1]));
+
+                members.AddMember(member);
+
+                membersCount--;
+            }
+
+            foreach (var member in members.People.OrderBy(m => m.Name))
+            {
+                if (member.Age > 30)
+                {
+                    Console.WriteLine($"{member.Name} - {member.Age}");
+                }
+            }
+        }
+    }
+}

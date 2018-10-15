f06341a4f502c166bb51333a880709ffa392d927
blazarow09 <blazarow09@students.softuni.bg>
2018-10-16 01:24:27 +0300

    upload CarSalesman solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/CarSalesman/Engine.cs
@@ -0,0 +1,69 @@
+﻿namespace CarSalesman
+{
+    using System.Text;
+
+    public class Engine
+    {
+        private string model;
+        private int power;
+        private int? displacement;
+        private string efficiency;
+
+        public string Efficiency
+        {
+            get { return efficiency; }
+            set { efficiency = value; }
+        }
+
+        public int? Displacement
+        {
+            get { return displacement; }
+            set { displacement = value; }
+        }
+
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public Engine (string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+
+        public Engine(string model, int power, int? displacement) :this(model, power)
+        {
+            this.Displacement = displacement;
+        }
+
+        public Engine(string model, int power, string efficiency) : this(model, power)
+        {
+            this.Efficiency = efficiency;
+        }
+
+        public Engine(string model, int power, int? displacement, string efficiency) : this(model, power)
+        {
+            this.Displacement = displacement;
+            this.Efficiency = efficiency;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"  {Model}:");
+            builder.AppendLine($"    Power: {Power}");
+            builder.AppendLine($"    Displacement: {(Displacement == null ? "n/a" : Displacement.ToString())}");
+            builder.AppendLine($"    Efficiency: {(Efficiency == null ? "n/a" : Efficiency.ToString())}");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/CarSalesman/StartUp.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarSalesman
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesOfEngines = int.Parse(Console.ReadLine());
+            List<Engine> engines = new List<Engine>();
+
+            for (int engine = 0; engine < linesOfEngines; engine++)
+            {
+                string[] tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[0];
+                int power = int.Parse(tokens[1]);
+
+
+                if (tokens.Length == 2)
+                {
+                    Engine engineInfo = new Engine(model, power);
+                    engines.Add(engineInfo);
+                }
+                else if (tokens.Length == 3)
+                {
+                    if (tokens[2].All(Char.IsDigit))
+                    {
+                        int displacement = int.Parse(tokens[2]);
+                        Engine engineInfo = new Engine(model, power, displacement);
+                        engines.Add(engineInfo);
+                    }
+                    else
+                    {
+                        string efficiency = tokens[2];
+                        Engine engineInfo = new Engine(model, power, efficiency);
+                        engines.Add(engineInfo);
+                    }
+                }
+                else if (tokens.Length == 4)
+                {
+                    int displacement = int.Parse(tokens[2]);
+                    string efficiency = tokens[3];
+                    Engine engineInfo = new Engine(model, power, displacement, efficiency);
+                    engines.Add(engineInfo);
+                }
+            }
+
+            int linesOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int car = 0; car < linesOfCars; car++)
+            {
+                string[] tokens = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[0];
+                string engineModel = tokens[1];
+
+                Engine engine = engines.First(e => e.Model == engineModel);
+
+                if (tokens.Length == 2)
+                {
+                    Car carInfo = new Car(model, engine);
+                    cars.Add(carInfo);
+                }
+                else if (tokens.Length == 3)
+                {
+                    if (tokens[2].All(Char.IsDigit))
+                    {
+                        int weight = int.Parse(tokens[2]);
+                        Car carInfo = new Car(model, engine, weight);
+                        cars.Add(carInfo);
+                    }
+                    else
+                    {
+                        string color = tokens[2];
+                        Car carInfo = new Car(model, engine, color);
+                        cars.Add(carInfo);
+                    }
+                }
+                else if (tokens.Length == 4)
+                {
+                    int weight = int.Parse(tokens[2]);
+                    string color = tokens[3];
+                    Car carInfo = new Car(model, engine, weight, color);
+                    cars.Add(carInfo);
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}

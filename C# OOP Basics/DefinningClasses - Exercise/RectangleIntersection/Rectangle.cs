559bd9947d1392b0890a9b1f8ac481a2728bc3eb
blazarow09 <blazarow09@students.softuni.bg>
2018-10-16 01:25:00 +0300

    upload RectangleIntersection solve

---
+++ C# OOP Basics/DefinningClasses - Exercise/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿namespace RectangleIntersection
+{
+    public class Rectangle
+    {
+        private string id;
+        private double width;
+        private double height;
+        private double x;
+        private double y;
+
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(string id, double width, double height, double x, double y)
+        {
+            this.id = id;
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool IsIntersects(Rectangle n)
+        {
+            
+
+            if ((n.y >= this.y && n.y - n.height <= this.y && n.x <= this.x && n.x + n.width >= this.x) ||
+            (n.y >= this.y && n.y - n.height <= this.y && n.x >= this.x && n.x <= this.x + this.width) ||
+            (n.y <= this.y && n.y >= this.y - this.height && n.x <= this.x && n.x + n.width >= this.x) ||
+            (n.y <= this.y && n.y >= this.y - this.height && n.x >= this.x && n.x <= this.x + this.width))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C# OOP Basics/DefinningClasses - Exercise/RectangleIntersection/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RectangleIntersection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var rectangleNums = int.Parse(tokens[0]);
+            var intersectionCount = int.Parse(tokens[1]);
+
+            var rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < rectangleNums; i++)
+            {
+                var rectangleInput = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var id = rectangleInput[0];
+                var width = double.Parse(rectangleInput[1]);
+                var height = double.Parse(rectangleInput[2]);
+                var x = double.Parse(rectangleInput[3]);
+                var y = double.Parse(rectangleInput[4]);
+
+                var rectangleInfo = new Rectangle(id, width, height, x, y);
+                rectangles.Add(rectangleInfo);
+            }
+
+            for (int j = 0; j < intersectionCount; j++)
+            {
+                var cheks = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var firstId = cheks[0];
+                string secondId = cheks[1];
+
+                var firstRectangle = rectangles.First(r => r.Id == firstId);
+                var secondRectangle = rectangles.First(r => r.Id == secondId);
+
+                if (firstRectangle.IsIntersects(secondRectangle))
+                    {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}

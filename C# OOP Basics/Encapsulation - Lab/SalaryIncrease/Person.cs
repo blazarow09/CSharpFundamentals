3f5483f802bd135200e637e920dd15b00240cdb9
blazarow09 <blazarow09@students.softuni.bg>
2018-10-18 01:46:09 +0300

    upload SalaryIncrease solve

---
+++ C# OOP Basics/Encapsulation - Lab/SalaryIncrease/Person.cs
@@ -0,0 +1,50 @@
+﻿namespace SalaryIncrease
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+        }
+
+        public decimal Salary
+        {
+            get { return this.salary; }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age > 30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
+        }
+    }
+}

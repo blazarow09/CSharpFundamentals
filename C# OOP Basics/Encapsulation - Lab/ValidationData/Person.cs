50cfba88afe3ba0b3ec5e05164069cfc869fd052
blazarow09 <blazarow09@students.softuni.bg>
2018-10-18 01:46:38 +0300

    upload ValidationData solve

---
+++ C# OOP Basics/Encapsulation - Lab/ValidationData/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace ValidationData
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age < 30)
+            {
+                this.salary += salary * percentage / 200;
+            }
+            else
+            {
+                this.salary += salary * percentage / 100;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} gets {this.salary:f2} leva.";
+        }
+    }
+}
---
+++ C# OOP Basics/Encapsulation - Lab/ValidationData/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidationData
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var countLines = int.Parse(Console.ReadLine());
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < countLines; i++)
+            {
+                var tokens = Console.ReadLine()
+                              .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    var person = new Person(
+                        tokens[0],
+                        tokens[1],
+                        int.Parse(tokens[2]),
+                        decimal.Parse(tokens[3]));
+
+                    persons.Add(person);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            var bonus = int.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p));
+        }
+    }
+}

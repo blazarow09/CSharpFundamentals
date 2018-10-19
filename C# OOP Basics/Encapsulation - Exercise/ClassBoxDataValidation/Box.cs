58ab43c5695e268daaecd9975b82513df0f71cef
blazarow09 <blazarow09@students.softuni.bg>
2018-10-20 01:26:39 +0300

    upload ClassBoxDataValidation solve

---
+++ C# OOP Basics/Encapsulation - Exercise/ClassBoxDataValidation/Box.cs
@@ -0,0 +1,94 @@
+﻿namespace ClassBoxDataValidation
+{
+    using System;
+
+    public class Box
+    {
+        private double volume;
+        private double surfaceArea;
+        private double lateralSurfaceArea;
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        private double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        private double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public void CalculateLateralSurfaceArea(double length, double width, double height)
+        {
+            this.lateralSurfaceArea = (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+        }
+
+        public void CalculateSurfaceArea(double length, double width, double height)
+        {
+            this.surfaceArea = (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+        }
+
+        public void CalculateVolume(double length, double width, double height)
+        {
+            this.volume = (this.Length * this.Width * this.Height);
+        }
+
+        public override string ToString()
+        {
+            return $"Surface Area - {this.surfaceArea:f2}\n" +
+                $"Lateral Surface Area - {this.lateralSurfaceArea:f2}\n" +
+                $"Volume - {this.volume:f2}";
+        }
+    }
+}
---
+++ C# OOP Basics/Encapsulation - Exercise/ClassBoxDataValidation/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassBoxDataValidation
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var boxes = new List<Box>();
+
+            var length = double.Parse(Console.ReadLine());
+
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                var box = new Box(length, width, height);
+
+                box.CalculateVolume(length, width, height);
+                box.CalculateSurfaceArea(length, width, height);
+                box.CalculateLateralSurfaceArea(length, width, height);
+                boxes.Add(box);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            boxes.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

8ff82bd0051658f74f6520faff4360a4e74f760a
blazarow09 <blazarow09@students.softuni.bg>
2018-10-20 01:26:26 +0300

    upload ClassBox solve

---
+++ C# OOP Basics/Encapsulation - Exercise/ClassBox/Box.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassBox
+{
+    public class Box
+    {
+        private double volume;
+        private double surfaceArea;
+        private double lateralSurfaceArea;
+
+        public Box()
+        {
+        }
+
+        public void CalculateLateralSurfaceArea(double length, double width, double height)
+        {
+            this.lateralSurfaceArea = (2 * length * height) + (2 * width * height);
+        }
+
+        public void CalculateSurfaceArea(double length, double width, double height)
+        {
+            this.surfaceArea = (2 * length * width) + (2 * length * height) + (2 * width * height);
+        }
+
+        public void CalculateVolume(double length, double width, double height)
+        {
+            this.volume = (length * width * height);
+        }
+
+        public override string ToString()
+        {
+            return $"Surface Area - {this.surfaceArea:f2}\n" +
+                $"Lateral Surface Area - {this.lateralSurfaceArea:f2}\n" +
+                $"Volume - {this.volume:f2}";
+        }
+    }
+}
---
+++ C# OOP Basics/Encapsulation - Exercise/ClassBox/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassBox
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var boxes = new List<Box>();
+
+            var length = double.Parse(Console.ReadLine());
+
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var box = new Box();
+
+            box.CalculateVolume(length, width, height);
+            box.CalculateSurfaceArea(length, width, height);
+            box.CalculateLateralSurfaceArea(length, width, height);
+            boxes.Add(box);
+
+            boxes.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

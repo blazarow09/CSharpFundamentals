9043d760be7b3dc5f2fd50d48773c14f49734354
blazarow09 <blazarow09@students.softuni.bg>
2018-10-20 22:16:26 +0300

    code cleaned up

--- C# OOP Basics/WorkingWithAbstraction - Exercise/Hospital/StartUp.cs
+++ C# OOP Basics/WorkingWithAbstraction - Exercise/Hospital/StartUp.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Hospital
+﻿namespace Hospital
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class StartUp
     {
         public static void Main()

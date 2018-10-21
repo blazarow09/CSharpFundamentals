7e0bf2cb81b6872868bd9f2c33a30135873827d2
blazarow09 <blazarow09@students.softuni.bg>
2018-10-22 01:09:21 +0300

    upload DefineAnInterfaceIPerson

---
+++ C# OOP Basics/Interfaces and Abstraction - Exercise/DefineAnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+        IPerson person = new Citizen(name, age);
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}

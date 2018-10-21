9eca1ecbfe6f48ed7fecdade6630a3fdfb5d8f74
blazarow09 <blazarow09@students.softuni.bg>
2018-10-22 01:18:37 +0300

    upload Person solve

---
+++ C# OOP Basics/Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+
+        protected override int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value >= 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ C# OOP Basics/Inheritance - Exercise/Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Person
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

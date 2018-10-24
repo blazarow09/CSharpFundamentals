99a15d15bf1b766b8c2f8574732aafe2695ba15b
blazarow09 <blazarow09@gmail.com>
2018-10-24 13:54:21 +0300

    upload GenericBox solve

---
+++ C# OOP Advanced/Generics - Exercise/GenericBox/Box.cs
@@ -0,0 +1,14 @@
+﻿public class Box<T>
+{
+    private T input;
+
+    public Box(T input)
+    {
+        this.input = input;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.input.GetType().FullName}: {this.input}";
+    }
+}
---
+++ C# OOP Advanced/Generics - Exercise/GenericBox/StartUp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(new Box<int>(123123));
+        Console.WriteLine(new Box<string>("life in a box"));
+    }
+}

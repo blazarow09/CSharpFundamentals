7b96a88f7156156c62be655ffb621ca9655e38e5
blazarow09 <blazarow09@gmail.com>
2018-10-24 14:18:17 +0300

    upload GenericBoxOfIntegers solve

---
+++ C# OOP Advanced/Generics - Exercise/GenericBoxOfIntegers/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        var count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            var value = int.Parse(Console.ReadLine());
+            Console.WriteLine(new Box<int>(value));
+        }
+    }
+}

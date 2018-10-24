0938d3e169a5d696b87a1481e42914cc456e8d51
blazarow09 <blazarow09@gmail.com>
2018-10-24 14:18:46 +0300

    GenericCountMethodDouble

---
+++ C# OOP Advanced/Generics - Exercise/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public class Box<T>
+   where T : IComparable<T>
+{
+    private List<T> values = new List<T>();
+    public Box(List<T> values)
+    {
+        this.values = values;
+    }
+    public int Count(double constraint)
+    {
+        var counter = 0;
+        foreach (var value in values)
+        {
+            if (constraint.CompareTo(value) < 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ C# OOP Advanced/Generics - Exercise/GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var count = int.Parse(Console.ReadLine());
+        var values = new List<double>();
+        for (int i = 0; i < count; i++)
+        {
+            var value = double.Parse(Console.ReadLine());
+            values.Add(value);
+        }
+        var constraint = double.Parse(Console.ReadLine());
+        Box<double> box = new Box<double>(values);
+        Console.WriteLine(box.Count(constraint));
+    }
+}

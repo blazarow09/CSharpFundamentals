ad627791100b7fdd0b3735a66ca5f8a6606d86da
blazarow09 <blazarow09@gmail.com>
2018-10-24 13:54:11 +0300

    uplaod CustomListSorter solve

---
+++ C# OOP Advanced/Generics - Exercise/CustomListSorter/CustomList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CustomList<T>
+where T : IComparable<T>
+{
+    private IList<T> elements;
+
+    public CustomList(IEnumerable<T> elements)
+    {
+        this.elements = new List<T>(elements);
+    }
+
+    public CustomList()
+        : this(Enumerable.Empty<T>())
+    {
+    }
+
+    public IEnumerable<T> Elements
+    {
+        get
+        {
+            return this.elements;
+        }
+    }
+
+    public void Sort()
+    {
+        var sorted = this.elements.ToList();
+        sorted.Sort();
+
+        this.elements =  sorted;
+    }
+
+    public void Add(T element)
+    {
+        this.elements.Add(element);
+    }
+
+    public T Remove(int index)
+    {
+        this.ValidateIndex(index);
+
+        T elementToRemove = this.elements[index];
+
+        this.elements.Remove(elementToRemove);
+
+        return elementToRemove;
+    }
+
+    public bool Contains(T element)
+    {
+        var contains = this.elements.Contains(element);
+
+        return contains;
+    }
+
+    public void Swap(int firstIndex, int secondIndex)
+    {
+        ValidateIndex(firstIndex);
+        ValidateIndex(secondIndex);
+
+        var temp = this.elements[firstIndex];
+
+        this.elements[firstIndex] = this.elements[secondIndex];
+        this.elements[secondIndex] = temp;
+    }
+
+    public int CountGreaterThan(T element)
+    {
+        var count = this.elements.Count(e => e.CompareTo(element) > 0);
+
+        return count;
+    }
+
+    public T Max()
+    {
+        return this.elements.Max();
+    }
+
+    public T Min()
+    {
+        return this.elements.Min();
+    }
+
+    private void ValidateIndex(int index)
+    {
+        if (index < 0 || index >= this.elements.Count)
+        {
+            throw new ArgumentException("Invalid index");
+        }
+    }
+}
+
---
+++ C# OOP Advanced/Generics - Exercise/CustomListSorter/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CustomListSorter
+{
+    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            CustomList<string> customList = new CustomList<string>();
+
+            while (line != "END")
+            {
+                var tokens = line.Split();
+                var command = tokens[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        customList.Add(tokens[1]);
+                        break;
+
+                    case "Remove":
+                        customList.Remove(int.Parse(tokens[1]));
+                        break;
+
+                    case "Contains":
+                        var count = customList.Contains(tokens[1]);
+                        Console.WriteLine(count);
+                        break;
+
+                    case "Swap":
+                        var firstIndex = int.Parse(tokens[1]);
+                        var secondIndex = int.Parse(tokens[2]);
+
+                        customList.Swap(firstIndex, secondIndex);
+                        break;
+
+                    case "Greater":
+                        var counter = customList.CountGreaterThan(tokens[1]);
+                        Console.WriteLine(counter);
+                        break;
+
+                    case "Max":
+                        var max = customList.Max();
+                        Console.WriteLine(max);
+                        break;
+
+                    case "Min":
+                        var min = customList.Min();
+                        Console.WriteLine(min);
+                        break;
+
+                    case "Sort":
+                        customList.Sort();
+                        break;
+
+                    case "Print":
+                        foreach (var element in customList.Elements)
+                        {
+                            Console.WriteLine(element);
+                        }
+                        break;
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}

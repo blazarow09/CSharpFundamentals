1512b1cf46bbe2ce74a95a9046aecce519c422ce
blazarow09 <blazarow09@gmail.com>
2018-10-24 14:19:02 +0300

    upload GenericCountMethodString

---
+++ C# OOP Advanced/Generics - Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+public class Box<T>
+   where T : IComparable
+{
+    private List<T> values = new List<T>();
+    public Box(List<T> values)
+    {
+        this.values = values;
+    }
+    public int Count(string constraint)
+    {
+        var counter = 0;
+        foreach (var value in values)
+        {
+            if (constraint.CompareTo(value) < 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
